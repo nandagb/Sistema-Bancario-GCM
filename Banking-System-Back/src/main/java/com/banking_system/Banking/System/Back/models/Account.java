a3857abbd4e2d754f876cf958ab0bc3a04a4b36a
nandagb <nandagb23@gmail.com>
2024-05-02 19:56:45 -0300

    criando modelo para conta

---
+++ Banking-System-Back/src/main/java/com/banking_system/Banking/System/Back/models/Account.java
@@ -0,0 +1,23 @@
+package com.banking_system.Banking.System.Back.models;
+
+public class Account {
+    private int number;
+
+    private int balance;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
