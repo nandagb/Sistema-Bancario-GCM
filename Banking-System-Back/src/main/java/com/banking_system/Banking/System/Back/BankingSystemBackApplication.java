ca8e255d886f14c6701ea2e879ff19b5a81826cb
nandagb <nandagb23@gmail.com>
2024-05-02 18:43:13 -0300

    adicionando projeto em spring

---
+++ Banking-System-Back/src/main/java/com/banking_system/Banking/System/Back/BankingSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.banking_system.Banking.System.Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemBackApplication.class, args);
+	}
+
+}
---
+++ Banking-System-Back/src/test/java/com/banking_system/Banking/System/Back/BankingSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking_system.Banking.System.Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
