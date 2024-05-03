8ae5aecb94ee93ff7fd8383ee9388773ee0fbc57
nandagb <nandagb23@gmail.com>
2024-05-03 15:00:56 -0300

    adicionando funcao para resgatar saldo da conta

--- Banking-System-Back/src/main/java/com/banking_system/Banking/System/Back/service/AccountService.java
+++ Banking-System-Back/src/main/java/com/banking_system/Banking/System/Back/service/AccountService.java
@@ -1,2 +1,27 @@
-package com.banking_system.Banking.System.Back.service;public class AccountService {
+package com.banking_system.Banking.System.Back.service;
+
+import com.banking_system.Banking.System.Back.models.Account;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class AccountService {
+    private List<Account> accounts = new ArrayList<Account>();
+
+    public int getAccountBalance(int accountNumber){
+        Boolean found = false;
+        for (Account account : accounts) {
+            if (account.getNumber() == accountNumber) {
+                found = true;
+                return account.getBalance();
+            }
+        }
+        if (!found){
+            throw new NoSuchElementException();
+        }
+        return -1;
+    }
 }
