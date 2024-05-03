95b91cca2bc5d2093438fc7a83be1b16ab331272
Ezequiel Morais <ezequielmorais4002@gmail.com>
2024-05-03 18:27:19 -0300

    criado cadastro de conta

--- banking-system-front/src/main.js
+++ banking-system-front/src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import SignUp from './components/pages/SignUp.vue'
+import IndexPage from './components/pages/IndexPage.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path:"/",
+        name:"index",
+        component:IndexPage
+    },
+    {
+        path:"/signup",
+        name:"signup",
+        component:SignUp
+    }
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+}
+)
+
+createApp(App).use(router).mount('#app')
