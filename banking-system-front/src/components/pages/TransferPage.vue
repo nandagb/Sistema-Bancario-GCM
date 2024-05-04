938e0148334b3858ab036ca730247481f6e969a4
nandagb <nandagb23@gmail.com>
2024-05-04 17:29:50 -0300

    adicionando feedback visual para a operação de transferência

--- banking-system-front/src/components/pages/TransferPage.vue
+++ banking-system-front/src/components/pages/TransferPage.vue
@@ -7,30 +7,36 @@
         <label for="value">Valor da Transferência</label>
         <input id="value" v-model="value" />
     </div>
+    <div v-if="sent">
+        <h1>
+            Operação Realizada com Sucesso!
+        </h1>
+    </div>
     <AppButton @click="handleTransfer(accountNumberOrigin, accountNumberDestination, value)">Transferir</AppButton>
 
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
 import AppButton from '../util/AppButton.vue';
 import { transfer } from '@/services/accountService.js'
+import {ref} from 'vue'
 
-let router = useRouter()
 const handleTransfer = async (accountNumberOrigin, accountNumberDestination, value) => {
 
     try {
         await transfer(accountNumberOrigin, accountNumberDestination, value)
-        router.back()
+        sent.value = true
     }
     catch(e){
-        console.error(e);
+        console.error(e)
+        sent.value = false
     }
 }
 
 let accountNumberOrigin
 let accountNumberDestination
 let value
+let sent = ref()
 </script>
 
 <style>
