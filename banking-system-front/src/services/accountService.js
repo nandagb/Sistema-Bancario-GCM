0c47323df1e730d5c52f65f8ee6869b164d6215e
nandagb <nandagb23@gmail.com>
2024-05-03 21:21:35 -0300

    adicionando requisicao ao service

--- banking-system-front/src/services/accountService.js
+++ banking-system-front/src/services/accountService.js
@@ -4,3 +4,7 @@ const backendURL= process.env.VUE_APP_ACCOUNT_BACKEND;
 export const createAccount = async (accountNumber) => {
     await axios.post(`${backendURL}/create`, {AccountNumber: parseInt(accountNumber)})
 }
+
+export const debit = async (accountNumber, debitValue) => {
+    await axios.post(`${backendURL}/debit`, {AccountNumber: parseInt(accountNumber), DebitValue: parseInt(debitValue)})
+}
