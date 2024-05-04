0636a26a3c4592cf99265bc17720ea39de0eff13
Ezequiel Morais <ezequielmorais4002@gmail.com>
2024-05-03 22:36:07 -0300

    adicionada tela de busca de saldo

--- banking-system-front/src/main.js
+++ banking-system-front/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 import SignUp from './components/pages/SignUp.vue'
 import IndexPage from './components/pages/IndexPage.vue'
+import BalancePage from './components/pages/BalancePage.vue'
 
 const routes = [
     {
@@ -14,6 +15,11 @@ const routes = [
         path:"/signup",
         name:"signup",
         component:SignUp
+    },
+    {
+        path:"/balance",
+        name:"balance",
+        component:BalancePage
     }
 ]
 
