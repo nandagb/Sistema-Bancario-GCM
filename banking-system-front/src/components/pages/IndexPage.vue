acdf2c4a467809cc2ab450e6e304df4e2d05cfe4
Ezequiel Morais <Ezequielmorais4002@gmail.com>
2024-05-14 17:39:52 -0300

    rel-2.2 (#48)
    * criando modelo para conta
    
    * criado projeto do front
    
    * corrigindo algumas dependencias
    
    * adicionada criação de contas
    
    * adicionando requisicao para consultar saldo
    
    * adicionando funcao para resgatar saldo da conta
    
    * mudado tipo esperado de int para Map<String,int>
    
    * corrigindo dependencia
    
    * Adicionada operação de crédito
    
    * adicionando requisição para debitar e lidar com exceções
    
    * criado cadastro de conta
    
    * Adicionada notação de Crossorigin no método de criar conta
    
    * Mudada mensagem de sucesso na requisição
    
    * adicionando botão para débito
    
    * adicionando rota para debito
    
    * adicionando pagina para debito
    
    * adicionando requisicao ao service
    
    * padronizando requisicao para debito e credito
    
    * adicionada tela de busca de saldo
    
    * criado botão de voltar
    
    * Adicionada notação de Crossorigin na requisição de recuperar saldo
    
    * corrigindo mais um conflito
    
    * consertando posicao do botao de debito
    
    * adicionando permissão de endereço ao endpoint
    
    * consertando value na requisicao de débito
    
    * adicionada tela de crédito
    
    * Adicionada notação de Crossorigin na requisição de crédito
    
    * adicionando requisição de transferência
    
    * adicionando funcao para realizar a transferência
    
    * removendo debug
    
    * adicionando modelo de requisicao transfer
    
    * adicionando botao e path de transferencia
    
    * adicionando página de transferência
    
    * removendo botão adicional
    
    * ajustando posicionamento do botão de transferência
    
    * corrigindo método da requisição transfer
    
    * adicionando feedback visual para a operação de transferência
    
    * impedindo que contas fiquem com saldo negativo
    
    * adicionando novo tipo de conta
    
    * adicionando atributo tipo para diferenciar as contas
    
    * adicionando método para criar conta poupança e atualizando o método para criar conta corrente
    
    * criando requisição e componente para criar conta poupança e atualizando a requisição de criar conta corrente
    
    * implementada conta bonus no back end
    
    * adicionando endpoint para render juros em todas as contas poupança
    
    * criado fluxo de débito para contas bonus
    
    * adicionada criação de contas bônus
    
    * adicionada informação do bônus ao pesquisar conta bônus
    
    * criando requisicao para render juros em contas poupança
    
    * Criado endpoint para recuperar informações da conta
    
    * Adicionado tipo de conta a conta corrente
    
    * consertado valor de bonus na consulta de saldo
    
    * consertada chamada do back na tela de débito
    
    * impedindo débito com valor negativo
    
    * impedindo crédito com valor negativo
    
    * impedindo transferência com valor negativo
    
    * mudada regra de bonificação
    
    ---------
    
    Co-authored-by: nandagb <nandagb23@gmail.com>
    Co-authored-by: Fernanda Gonçalves Barros <69534090+nandagb@users.noreply.github.com>

--- banking-system-front/src/components/pages/IndexPage.vue
+++ banking-system-front/src/components/pages/IndexPage.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <RouterLink to="/signup">
-      <AppButton>Criar Conta</AppButton>
+    <RouterLink to="/signup/current">
+      <AppButton>Criar Conta Corrente</AppButton>
+    </RouterLink>
+  </div>
+
+  <div>
+    <RouterLink to="/signup/savings">
+      <AppButton>Criar Conta Poupanca</AppButton>
+    </RouterLink>
+  </div>
+
+  <div>
+    <RouterLink to="/signup/bonus_account">
+      <AppButton>Criar Conta Bônus</AppButton>
     </RouterLink>
   </div>
     
@@ -27,6 +39,12 @@
   <div>
     <RouterLink to="/credit">
       <AppButton>Adicionar Crédito</AppButton>
+    </RouterLink>
+  </div>
+
+  <div>
+    <RouterLink to="/yield_interest">
+      <AppButton>Render Juros</AppButton>
     </RouterLink>
   </div>
 </template>
